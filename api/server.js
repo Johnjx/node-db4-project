289964df52ca46f1597ec219d6bed7c4506c96d9
John Ruddock <john.jxr@pm.me>
2022-06-16 15:26:50 -0400

    recipes router set

--- api/server.js
+++ api/server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const recipesRouter = require('./recipes_resource/recipes-router')
 
 const server = express();
-
 server.use(express.json());
+server.use('/api/recipes', recipesRouter)
 
 server.use('*', (req, res) => {
     res.status(404).json({ message: '404 Not Found'})
